8491037eefe75c18e5ca0c04f34f3ad3b027a7d6
Vinícius Girotto <vinicius.victor.girotto@gmail.com>
2019-09-07 22:42:09 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:local_sales/screens/cadastro.dart';
 
-void main(){
-  runApp(MaterialApp(home: Home())
-  );
-}
+void main() => runApp(MyApp());
 
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView(
-        padding: EdgeInsets.only(left: 25.0, right: 25.0),
-        children:<Widget>[
-          Text('Sales Local', style: TextStyle(fontSize: 40), textAlign: TextAlign.center),
-          Container(
-            padding: EdgeInsets.only(top: 50.0),
-            child:
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: 'Usuario',
-                prefixIcon: Icon(Icons.account_circle, size: 40, ),
-              ),
-            ),
-
-          ),
-          Container(
-            padding: EdgeInsets.only(bottom: 50.0),
-            child:
-            TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    labelText: 'Senha',
-                    prefixIcon: Icon(Icons.lock_outline, size: 40, )
-                )
-            ),
-          ),
-
-          Checkbox(//////////////////////
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                //labelText: 'Forma de pagamento',
-                  prefixIcon: Icon(Icons.attach_money, size: 40, )
-              )
-          )
-
-
-
-          //, style: TextStyle(color: Colors.white, fontSize: 40))//,
-          //Text('Login', style: TextStyle(color: Colors.white,fontSize: 20, height: 10.0)),
-          //Text('Senha', style: TextStyle(color: Colors.white,fontSize: 20, height: 5.0)),
-          //Text('email', style: TextStyle(color: Colors.white, fontSize: 20, height: 5.0)),
-          //Text('Informações pessoais', style: TextStyle(color: Colors.white, fontSize: 40, height:5.0)),
-          //Text('Formas de pagametno', style: TextStyle(color: Colors.white,fontSize: 20, height: 5.0))
-
-
-
-
-
-        ]
-    )
-    );}
+    return MaterialApp(
+        title: 'LocalSales',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Cadastro()
+    );
+  }
 }
---
+++ lib/screens/cadastro.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Cadastro extends StatefulWidget {
+  @override
+  _CadastroState createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  final _formKey = GlobalKey<FormState>();
+  String _pass = "";
+  bool isSwitched = false;
+  String _img_path;
+
+  final date = new RegExp(r"\d\d[^\w]\d\d[^\w]\d\d\d\d");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: ListView(
+            padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 20.0),
+            children: <Widget>[
+              Container(
+                  child: new Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                          width: 150.0,
+                          height: 150.0,
+                          decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: AssetImage("lib/assets/Logo.png")))))),
+              Container(
+                child: new Text('LocalSales',
+                    style: TextStyle(fontSize: 40, color: Colors.orange),
+                    textAlign: TextAlign.center),
+              ),
+              Container(
+                  child: new TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: new InputDecoration(
+                  labelStyle: TextStyle(color: Colors.black),
+                  hintText: 'you@example.com',
+                  labelText: "E-mail Adress",
+                  prefixIcon: Icon(
+                    Icons.account_circle,
+                    size: 30,
+                    color: Colors.orange,
+                  ),
+                ),
+                validator: (text) {
+                  if (text.isEmpty || !text.contains("@"))
+                    return "E-mail inválido!";
+                },
+              )),
+              Container(
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  //obscureText: true,
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.black),
+                      labelText: 'Password',
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        size: 30,
+                        color: Colors.orange,
+                      )),
+                  validator: (_pass) {
+                    if (_pass.isEmpty || _pass.length < 6)
+                      return "Senha inválida!";
+                  },
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  //obscureText: true,
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.black),
+                      labelText: 'Reenter the Password',
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        size: 30,
+                        color: Colors.orange,
+                      )),
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6) {
+                      return "Senha inválida";
+                    } else if (text != _pass) {
+                      return "As senhas não conferem!";
+                    }
+                  },
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Row(children: <Widget>[
+                    Icon(
+                      Icons.account_box,
+                      size: 35,
+                      color: Colors.orange,
+                    ),
+                    Text(
+                      "Informações Pessoais",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    )
+                  ])),
+              Container(
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintText: "Fulano da Silva",
+                      labelText: 'Display Name',
+                      prefixIcon: Icon(
+                        Icons.contacts,
+                        size: 30,
+                        color: Colors.orange,
+                      )),
+                  validator: (text) {
+                    if (text.isEmpty) return "Nome Inválido!";
+                  },
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  keyboardType: TextInputType.datetime,
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintText: "01/01/2000",
+                      labelText: 'Birthdate',
+                      prefixIcon: Icon(
+                        Icons.cake,
+                        size: 30,
+                        color: Colors.orange,
+                      )),
+                  validator: (text) {
+                    if (text.isEmpty || (!date.hasMatch(text)))
+                      return "Data Inválida!";
+                  },
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      child: Container(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            width: 100.0,
+                            height: 100.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: _img_path != null
+                                    ? AssetImage(_img_path)
+                                    : NetworkImage(
+                                        "https://logodetimes.com/wp-content/uploads/corinthians-capa.jpg"),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            child: SimpleDialog(
+                              title: Text("Salveee"),
+                              children: <Widget>[
+                                Text(
+                                  "Só mostrando que aqui vai abrir a galeria pra trocar a foto :D",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                  ),
+                                )
+                              ],
+                            ));
+                      },
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      child: Text(
+                        "User Photo",
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.black,
+                        ),
+                      ),
+                    )
+
+                  ],
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Row(children: <Widget>[
+                    Icon(
+                      Icons.attach_money,
+                      size: 35,
+                      color: Colors.orange,
+                    ),
+                    Text(
+                      "Métodos de Pagamento",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    )
+                  ])),
+              Container(
+                  child: new Row(
+                children: <Widget>[
+                  Text("PicPay", textAlign: TextAlign.left),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Switch(
+                      value: isSwitched,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitched = value;
+                        });
+                      },
+                      activeTrackColor: Colors.orangeAccent,
+                      activeColor: Colors.orange,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200.0,
+                    child: TextFormField(
+                      enabled: isSwitched,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        hintText: 'PicPay user',
+                      ),
+                      validator: (text) {
+                        if (text.isEmpty) return "Conta Inválida!";
+                      },
+                    ),
+                  ),
+                ],
+              )),
+              Container(
+                padding: EdgeInsets.only(top: 20.0),
+                child: SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    child: Text(
+                      "Criar Conta",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    textColor: Colors.black,
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {}
+                    },
+                  ),
+                ),
+              ),
+            ]),
+      ),
+
+      //, style: TextStyle(color: Colors.white, fontSize: 40))//,
+      //Text('Senha', style: TextStyle(color: Colors.white,fontSize: 20, height: 5.0)),
+      //Text('email', style: TextStyle(color: Colors.white, fontSize: 20, height: 5.0)),
+      //Text('Informações pessoais', style: TextStyle(color: Colors.white, fontSize: 40, height:5.0)),
+      //Text('Formas de pagamento', style: TextStyle(color: Colors.white,fontSize: 20, height: 5.0))
+      backgroundColor: Colors.white,
+    );
+  }
+}
