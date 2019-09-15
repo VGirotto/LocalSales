2c9a2af1b895e40260c521cc6389e986964c95a2
Claudio <claudiojlf@gmail.com>
2019-09-15 14:23:11 -0300

    degrade na homepage

--- local_sales/lib/tabs/home_tab.dart
+++ local_sales/lib/tabs/home_tab.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    
+    Widget _buildBodyBack() => Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 255, 124, 39),
+            Colors.white
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        )
+      ),
+    );
+    
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack()
+      ],
     );
   }
 }
