e0cb7913430c6af4edbaa2af0eff997c2924d01a
Gabriel Kenji de Almeida <gab_kenji>
2019-10-05 19:36:48 -0300

    Páginas de produto finalizada, erro na inserção
    do banco de dados

--- local_sales/lib/blocs/product_bloc.dart
+++ local_sales/lib/blocs/product_bloc.dart
@@ -1,11 +1,17 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:rxdart/rxdart.dart';
 
 class ProductBloc extends BlocBase {
 
   final _dataController = BehaviorSubject<Map>();
+  final _loadingController = BehaviorSubject<bool>();
+  final _createdController = BehaviorSubject<bool>();
+
   Stream<Map> get outData => _dataController.stream;
+  Stream<bool> get outLoading => _loadingController.stream;
+  Stream<bool> get outCreated => _createdController.stream;
 
   String categoriaID;
   DocumentSnapshot product;
@@ -16,10 +22,14 @@ class ProductBloc extends BlocBase {
     if(product != null){
       unsavedData = Map.of(product.data);
       unsavedData['images'] = List.of(product.data["images"]);
+
+      _createdController.add(true);
     } else {
       unsavedData = {
-        "title": null, "description": null, "price": null, "images": [], "amount": null, "name": null,
+        "title": null, "description": null, "price": null, "images": [], "amount": null, "uid": null,
       };
+
+      _createdController.add(false);
     }
     _dataController.add(unsavedData);
 
@@ -41,9 +51,55 @@ class ProductBloc extends BlocBase {
     unsavedData["images"] = images;
   }
 
+  Future<bool> saveProduct() async {
+    _loadingController.add(true);
+
+    try{
+      if(product != null){
+        await _uploadImages (product.documentID);
+        await product.reference.updateData(unsavedData);
+      } else {
+        DocumentReference dr = await Firestore.instance.collection("products")
+            .document(categoriaID).collection("items").add(Map.from(unsavedData)..remove("images"));
+        await _uploadImages(dr.documentID);
+        await dr.updateData(unsavedData);
+      }
+
+      _createdController.add(true);
+      _loadingController.add(false);
+      return true;
+
+    } catch(e){
+      _loadingController.add(false);
+      return false;
+    }
+  }
+
+  Future _uploadImages(String productID) async {
+    for(int i = 0; i < unsavedData["images"].length; i++){
+      if(unsavedData["images"][i] is String) continue;
+
+      StorageUploadTask uploadTask = FirebaseStorage.instance.ref().child(categoriaID).
+      child(productID).child(DateTime.now().millisecondsSinceEpoch.toString()).
+      putFile(unsavedData["images"][i]);
+
+      StorageTaskSnapshot s = await uploadTask.onComplete;
+      String downloadUrl = await s.ref.getDownloadURL();
+
+      unsavedData["images"][i] = downloadUrl;
+    }
+
+  }
+
+  void deleteProduct(){
+    product.reference.delete();
+
+  }
 
   @override
   void dispose(){
     _dataController.close();
+    _loadingController.close();
+    _createdController.close();
   }
 }
