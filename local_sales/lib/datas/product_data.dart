4eb6107e064cf9350e67dc63f74d7f1f4eca0032
augustocrocha <augusto.rochacampos@gmail.com>
2019-10-07 01:21:05 -0300

    Product Screen adicionada.

--- local_sales/lib/datas/product_data.dart
+++ local_sales/lib/datas/product_data.dart
@@ -6,14 +6,19 @@ class ProductData{
   String title;
   String description;
   double price;
+  int amount;
   List images;
   List size;
+  String salesperson;
 
   ProductData.fromDocument(DocumentSnapshot snapshot){
     id = snapshot.documentID;
     title  = snapshot.data["title"];
-    description = snapshot.data["descricao"];
-    price = snapshot.data["price"] + 0.0;
+    description = snapshot.data["description"];
+    price = snapshot.data["price"]+0.0;
     images = snapshot.data["images"];
+    amount = snapshot.data["amount"];
+    salesperson = snapshot.data["vendedor"];
+
   }
 }
--- local_sales/lib/screens/product_screen.dart
+++ local_sales/lib/screens/product_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:local_sales/datas/product_data.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class ProductScreen extends StatefulWidget {
-
   final ProductData product;
 
   ProductScreen(this.product);
@@ -12,13 +12,121 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-
   final ProductData product;
 
   _ProductScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme
+        .of(context)
+        .primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 0.9,
+                child: Carousel(
+                    images: product.images.map((url) {
+                      return NetworkImage(url);
+                    }).toList(),
+                    dotSize: 4.0,
+                    dotSpacing: 10.0,
+                    dotBgColor: Colors.transparent,
+                    dotColor: Colors.grey,
+                    dotIncreasedColor: Colors.deepOrange,
+                    autoplay: false),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+                      child: Text(
+                        product.title,
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.w500),
+                        maxLines: 3,
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Text(
+                          "R\$ ${product.price.toStringAsFixed(2)}",
+                          style: TextStyle(
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor),
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 35.0, 0.0, 0.0),
+                        child: Text(
+                          "Descrição",
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.grey),
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 0.0),
+                        child: Text(product.description,
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w500
+                          ),
+                        )
+
+                    ),
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                        child: Text(
+                          "Quantidade Disponível: " + product.amount.toString(),
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300
+                          ),
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                        child: Text(
+                          "Vendedor(a): " + product.salesperson.toString(),
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300
+                          ),
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+                        child: RaisedButton(
+                          color: Colors.deepOrange ,
+                          onPressed: (){},
+                          child: Text("Falar com Vendedor(a)",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white,
+                                fontSize: 18.0
+                            ),
+                          ),
+                        )
+                    )
+
+                  ],
+                ),
+              )
+            ],
+          )
+      ),
+    );
   }
 }
