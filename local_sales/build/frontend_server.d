bc8c5af76f5a74b10490c1a9619bb7db10983d38
Claudio <claudiojlf@gmail.com>
2019-10-12 22:22:48 -0300

    pesquisa funcionando parcialmente

--- local_sales/build/frontend_server.d
+++ local_sales/build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /home/augusto/flutter-sdk/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
+frontend_server.d: C:\src\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
--- local_sales/lib/tabs/home_tab.dart
+++ local_sales/lib/tabs/home_tab.dart
@@ -14,26 +14,35 @@ class _HomeTabState extends State<HomeTab>{
   Widget _tituloAppBar = new Text( 'Produtos' );
   CollectionReference _firestore = Firestore.instance.collection("Produtos").document("Todos").collection("itens");
   Future<QuerySnapshot> _resultado = Firestore.instance.collection("Produtos").document("Todos").collection("itens").getDocuments();
+  //Query where().firestore;
 
   @override
   Widget build(BuildContext context) {
 
-    Query _filter(String search){
-      CollectionReference produtosFiltrados = this._firestore;
+    /*Query _filter(String search){
+      setState(() {
+        CollectionReference produtosFiltrados = this._firestore;
+
+        this.saida = produtosFiltrados.where('itens.title', isEqualTo: search);  
+      });
+      /*CollectionReference produtosFiltrados = this._firestore;
       Query saida;
 
-      saida = produtosFiltrados.where('title'.toUpperCase(), arrayContains: search.toUpperCase());
+      saida = produtosFiltrados.where('itens.title', isEqualTo: search);*/
       
       return saida;
-    }
+    }*/
 
     void onChangedSearch(String search){
-      if(search.trim().isEmpty){
-        this._resultado = this._firestore.getDocuments();
-        print("ok");
-      }else{
-        this._resultado = _filter(search.trim()).getDocuments();
-      }
+      setState(() {
+        if(search.trim().isEmpty){
+          this._resultado = this._firestore.getDocuments();
+          print("ok");
+        }else{
+          this._resultado = this._firestore.where('title', isEqualTo: search).getDocuments();
+          print(search);
+        }  
+      });
     }
 
     void _searchPressed(){
@@ -45,7 +54,7 @@ class _HomeTabState extends State<HomeTab>{
               prefixIcon: new Icon(Icons.search),
               hintText: 'Pesquisa...'
             ),
-            onChanged: onChangedSearch,
+            onChanged: onChangedSearch
           );
         }else{
           this._icone = new Icon(Icons.search);
