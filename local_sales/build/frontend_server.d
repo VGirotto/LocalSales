0e2330e50305b95f2494831f7b8efdeae7475fb7
Gabriel Kenji de Almeida <gab_kenji>
2019-10-26 10:38:59 -0300

    Notificações teste

--- local_sales/build/frontend_server.d
+++ local_sales/build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /home/beatriz/development/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
+frontend_server.d: C:\Users\gab_k\src\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
--- local_sales/lib/main.dart
+++ local_sales/lib/main.dart
@@ -1,30 +1,32 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:local_sales/models/user_model.dart';
-import 'package:local_sales/screens/login_screen.dart';
-import 'package:scoped_model/scoped_model.dart';
-import 'package:local_sales/screens/feed_screen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'MyApp.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  final String currentUserId;
+
+  MainScreen({Key key, @required this.currentUserId}) : super(key: key);
+
+  @override
+  State createState() => MainScreenState(currentUserId: currentUserId);
+}
+
+class _MessageHandler extends StatefulWidget {
+  @override
+  __MessageHandlerState createState() => __MessageHandlerState();
+}
+
+class __MessageHandlerState extends State<_MessageHandler> {
   @override
   Widget build(BuildContext context) {
-    return ScopedModel<UserModel>(
-        model: UserModel(),
-        child: MaterialApp(
-            title: 'Local Sales',
-            theme: ThemeData(
-              primarySwatch: Colors.orange,
-            ),
-            debugShowCheckedModeBanner: false,
-            home: ScopedModelDescendant<UserModel>(
-              builder: (context, child, model){
-                if(model.isLoggedIn())
-                  return Feed();
-                else
-                  return Login();
-              },
-            ),
-        ),
-    );
+    return Container();
   }
 }
+
