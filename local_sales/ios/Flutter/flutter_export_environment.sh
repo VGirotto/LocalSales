1b13283e58bad26f67d8d011c5b7e6eacfe4b487
Beatriz Martins Angelo <bm.angelo@unifesp.br>
2019-11-02 11:07:46 -0300

    commite

--- local_sales/ios/Flutter/flutter_export_environment.sh
+++ local_sales/ios/Flutter/flutter_export_environment.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
-export "FLUTTER_ROOT=/home/vinicius/Documentos/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/vinicius/AndroidStudioProjects/LocalSales/Kenji/LocalSales/local_sales"
+export "FLUTTER_ROOT=/home/beatriz/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/beatriz/repositorio/LocalSales/local_sales"
 export "FLUTTER_TARGET=lib/main.dart"
-=======
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Claud\Documents\EngSoftw\LocalSales\local_sales"
-export "FLUTTER_TARGET=lib\main.dart"
->>>>>>> origin
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/beatriz/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
