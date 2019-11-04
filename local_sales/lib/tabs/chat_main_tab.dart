3b3f3b79757df504b0c0e64eb0dea6b94bf99c33
Vinícius Girotto <vinicius.victor.girotto@gmail.com>
2019-11-04 16:22:06 -0300

    Exibição de Usuário que já conversou

--- local_sales/lib/tabs/chat_main_tab.dart
+++ local_sales/lib/tabs/chat_main_tab.dart
@@ -1,15 +1,16 @@
-import 'dart:convert';
+    import 'dart:convert';
 import 'dart:core' as prefix0;
 import 'dart:core';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:local_sales/models/user_model.dart';
 import 'package:local_sales/tabs/chat_tab.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:scoped_model/scoped_model.dart';
 
 class chat_main extends StatefulWidget {
 
@@ -98,7 +99,7 @@ class _chat_mainState extends State<chat_main>{
     print("entrou com 1 - $name");
     print("entrou com 2 - $currentUserId");
     return Container(
-      child: StreamBuilder(
+      child: StreamBuilder (
         stream: Firestore.instance.collection('users').snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
@@ -107,10 +108,10 @@ class _chat_mainState extends State<chat_main>{
                 valueColor: AlwaysStoppedAnimation<Color>(themeColor),
               ),
             );
-          } else {
+          } else{
             return ListView.builder(
-              padding: EdgeInsets.all(10.0),
-              itemBuilder: (context, index) => buildItem(context, snapshot.data.documents[index]),
+              //padding: EdgeInsets.all(10.0),
+              itemBuilder: (context, index) => buildItem (context, snapshot.data.documents[index]),
               itemCount: snapshot.data.documents.length,
             );
           }
@@ -119,78 +120,98 @@ class _chat_mainState extends State<chat_main>{
     );
   }
 
-
   Widget buildItem(BuildContext context, DocumentSnapshot document) {
-    //print(document['name']);
-    return document['name'] == name ? Container() : Container(
-        child: OutlineButton(
-          child: Row(
-            children: <Widget>[
-              Material(
-                child: document['photoUrl'] != null
-                    ? CachedNetworkImage(
-                  placeholder: (context, url) => Container(
-                    child: CircularProgressIndicator(
-                      strokeWidth: 1.0,
-                      valueColor: AlwaysStoppedAnimation<Color>(themeColor),
-                    ),
-                    width: 50.0,
-                    height: 50.0,
-                    padding: EdgeInsets.all(15.0),
-                  ),
-                  imageUrl: document['photoUrl'],
-                  width: 50.0,
-                  height: 50.0,
-                  fit: BoxFit.cover,
-                )
-                    : Icon(
-                  Icons.account_circle,
-                  size: 50.0,
-                  color: Colors.orange,
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                clipBehavior: Clip.hardEdge,
-                color: Colors.deepOrangeAccent,
-              ),
-              Flexible(
-                child: Container(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          '${document['name']}',
-                          style: TextStyle(color: Colors.deepOrangeAccent,
-                            decorationColor: Colors.black,
-                            fontSize: 16.0,
-                          ),
-                        ),
-                        alignment: Alignment.centerLeft,
-                        margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
-                      ),
-                    ],
-                  ),
-                  margin: EdgeInsets.only(left: 20.0),
-                ),
-              ),
-            ],
-          ),
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => Chat(
-                      peerId: document.documentID,
-                      peerAvatar: document['photoUrl'],
-                      namePressed: document.data['name'],
-                      id: currentUserId,
-                    )));
-          },
-          color: Colors.transparent,
-          padding: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        ),
-        margin: EdgeInsets.only(bottom: 10.0, left: 5.0, right: 5.0),
-      );
-  }
+    return Container(
+      child: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model){
+          List ids = new List();
+          ids = model.userData["conversou"];
+          int aux = 0;
+          int i;
+          for(i = 0;i < ids.length;i++){
+            if(document['id'] == ids[i]) aux=1;
+          }
 
+          if (aux == 1) {
+            return Container(
+              child: OutlineButton(
+                child: Row(
+                  children: <Widget>[
+                    Material(
+                      child: document['photoUrl'] != null
+                          ? CachedNetworkImage(
+                        placeholder: (context, url) => Container(
+                          child: CircularProgressIndicator(
+                            strokeWidth: 1.0,
+                            valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                          ),
+                          width: 50.0,
+                          height: 50.0,
+                          padding: EdgeInsets.all(15.0),
+                        ),
+                        imageUrl: document['photoUrl'],
+                        width: 50.0,
+                        height: 50.0,
+                        fit: BoxFit.cover,
+                      )
+                          : Icon(
+                        Icons.account_circle,
+                        size: 50.0,
+                        color: Colors.orange,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                      clipBehavior: Clip.hardEdge,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    Flexible(
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                '${document['name']}',
+                                style: TextStyle(color: Colors.deepOrangeAccent,
+                                  decorationColor: Colors.black,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                              alignment: Alignment.centerLeft,
+                              margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
+                            ),
+                          ],
+                        ),
+                        margin: EdgeInsets.only(left: 20.0),
+                      ),
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Chat(
+                            peerId: document.documentID,
+                            peerAvatar: document['photoUrl'],
+                            namePressed: document.data['name'],
+                            id: currentUserId,
+                          )));
+                },
+                color: Colors.transparent,
+                padding: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              ),
+              margin: EdgeInsets.only(left: 5.0, right: 5.0, top: 10.0),
+            );
+          } else {
+            return Container();
+          }
+
+        }
+      ),
+    
+    );
+
+    //DocumentSnapshot future = await Firestore.instance.collection("users").document(currentUserId).get();
+    
+  }
 }
